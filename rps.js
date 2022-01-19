644c12f573209d33dd03eab8d3aa1ff9b4bb4d1c
kondrei <kondrei@gmail.com>
2022-01-19 21:16:42 +0200

    rock paper scissors The Odin Project game

--- rps.js
+++ rps.js
@@ -2,8 +2,17 @@ const gameChoose = ['Rock', 'Paper', 'Scissors']
 let computerChoose = document.getElementById('computer')
 let playerChoose = document.getElementById('human');
 
-let computerScore, humanScore = 0;
+let computerScore = 0;
+let playerScore = 0;
 
+let computerScoreHolder = document.querySelector('#computerScore');
+let playerScoreHolder = document.querySelector('#playerScore');
+updateScore();
+
+function updateScore() {
+    computerScoreHolder.innerText = computerScore;
+    playerScoreHolder.innerText = playerScore;
+}
 function computerPlay() {
     resetCss(computerChoose);
     let randomChoose = Math.floor(Math.random() * 3)
@@ -16,7 +25,6 @@ function computerPlay() {
 }
 
 
-
 function resetCss(element) {
     for (let item = 0; item < element.children.length; item++) {
         element.children[item].style.backgroundColor = 'white'
@@ -26,20 +34,53 @@ function startGame() {
 
     for (let item = 0; item < playerChoose.children.length; item++) {
         playerChoose.children[item].addEventListener('click', (e) => {
+            let computerChoose = computerPlay();
             resetCss(playerChoose)
             e.target.style.backgroundColor = '#c2dbb4';
             // if (gameChoose.indexOf(e.target.innerHTML) === computerPlay()) {
             //     console.log('catch')
             // }
-            console.log(item)
-            if ((computerPlay() === item))
-                console.log('egal')
-            if ((computerPlay() === 0) && item > 0) {
-                console.log('computer wins')
+            if ((computerChoose === item)) { //draw
+                computerScore++;
+                playerScore++;
             }
+            // ['Rock', 'Paper', 'Scissors']
+            if ((computerChoose === 0) && item === 2) {  //rock > scissors
+                computerScore++;
+            }
+
+            if ((computerChoose === 2) && item === 0) {  //scissors > rock
+                playerScore++;
+            }
+
+            if ((computerChoose === 2) && item === 1) {  //Scissors > Paper
+                computerScore++;
+            }
+            if ((computerChoose === 1) && item === 2) {  // Paper> Scissors
+                playerScore++;
+            }
+
+            if ((computerChoose === 1) && item === 0) {  // paper > rock
+                computerScore++;
+            }
+
+            if ((computerChoose === 0) && item === 1) {  // paper > rock
+                playerScore++;
+            }
+
+            updateScore();
         })
     }
 
 }
 
+let reset = document.querySelector('button')
+
+reset.addEventListener('click', () => {
+    computerScore = 0;
+    playerScore = 0;
+    updateScore();
+    resetCss(playerChoose)
+    resetCss(computerChoose)
+})
 startGame()

6445a65f2986bb5418d3ff2f95b11c30f650244e
kondrei <kondrei@gmail.com>
2022-01-26 14:13:28 +0000

    DOM exercises

---
+++ DOM/script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('#container')
+
+function creteDomElement(type, context = '', parent) {
+    let element = document.createElement(type);
+    element.textContent = context
+    parent.appendChild(element)
+    return element;
+}
+
+const para = creteDomElement('p', 'Hey I\’m red', container)
+
+const heading = creteDomElement('h3', 'I’m a blue h3!', container)
+
+const div = creteDomElement('div', '', container)
+div.style.backgroundColor = 'pink'
+
+const heading2 = creteDomElement('h2', 'I\’m in a div', div)
+
+const para2 = creteDomElement('p', 'mee too', div)
+
+const button = document.querySelector('#btn')
+button.addEventListener('click', (e) => {
+    console.log('you click' + e.target)
+})

1d22f3b1049b742151ca85f14e03ccfd33d5b280
kondrei <kondrei@gmail.com>
2022-01-29 08:10:23 +0000

    Sketch project

--- Sketch/app.js
+++ Sketch/app.js
@@ -1,11 +1,20 @@
 export class Sketck {
-    constructor() {
+    constructor(gridSize) {
+        this.parent = document.querySelector('.grid-container')
+        this.gridSize = gridSize
         console.log('constructor loaded')
         this.startGame()
     }
 
     startGame() {
-        let button = document.getElementById('btn');
-        console.log(button)
+        // let button = document.getElementById('btn');
+        this.addElements();
+    }
+
+    addElements() {
+        for (let i = 0; i < this.gridSize; i++) {
+            let element = document.createElement('div')
+            this.parent.appendChild(element);
+        }
     }
 }

0421e52c748e28356fd8853eb743dfc4771f5bec
kondrei <kondrei@gmail.com>
2022-01-31 07:22:24 +0200

    making grid responsive

--- Sketch/app.js
+++ Sketch/app.js
@@ -13,8 +13,13 @@ export class Sketck {
 
     addElements() {
         for (let i = 0; i < this.gridSize; i++) {
-            let element = document.createElement('div')
-            this.parent.appendChild(element);
+            this.parent.style.gridTemplateColumns = `repeat(${this.gridSize}, minmax(20px,1fr))`;
+            this.parent.style.gridTemplateRows = `repeat(${this.gridSize}, minmax(20px,1fr))`;
+            for (let x = 0; x < this.gridSize; x++) {
+
+                let element = document.createElement('div')
+                this.parent.appendChild(element);
+            }
         }
     }
 }

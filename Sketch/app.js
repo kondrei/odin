0f95e4ef8c60ef258feb6bf2175a611ad36a13c0
kondrei <kondrei@gmail.com>
2022-01-31 16:22:26 +0200

    Change grid size and get random color

--- Sketch/app.js
+++ Sketch/app.js
@@ -2,33 +2,58 @@ export class Sketck {
     constructor(gridSize) {
         this.parent = document.querySelector('.grid-container')
         this.button = document.querySelector('#btn')
+        this.color = document.querySelector('#color')
+        this.setGridSize = document.querySelector('#gridSize')
         this.gridSize = gridSize
-        console.log('constructor loaded')
+        this.output = document.querySelector('#output')
+        this.output.textContent = this.gridSize + ' x ' + this.gridSize
+        this.rainbow = document.querySelector('#rainbow')
         this.startGame()
     }
 
     startGame() {
         this._addElements();
         this.button.addEventListener('click', () => { this._resetGrid() })
+        this.setGridSize.addEventListener('input', () => { this._resizeGrid() })
     }
 
     _addElements() {
+        while (this.parent.lastChild) {
+            this.parent.lastChild.remove()
+        }
         for (let i = 0; i < this.gridSize * this.gridSize; i++) {
-            this.parent.style.gridTemplateColumns = `repeat(${this.gridSize}, minmax(20px,1fr))`;
-            this.parent.style.gridTemplateRows = `repeat(${this.gridSize}, minmax(20px,1fr))`;
+            this.parent.style.gridTemplateColumns = `repeat(${this.gridSize}, minmax(5px,1fr))`;
+            this.parent.style.gridTemplateRows = `repeat(${this.gridSize}, minmax(5px,1fr))`;
 
             let element = document.createElement('div')
-            element.addEventListener('mouseover', () => {
-                element.style.backgroundColor = '#6e7e85'
+            element.addEventListener('mouseenter', () => {
+                element.style.backgroundColor = this._color()
             })
             this.parent.appendChild(element);
         }
     }
 
+    _color() {
+        if (!rainbow.checked) {
+            return this.color.value
+        }
+        else {
+            let randomColor = `hsl(${Math.random() * 360}, 50%, 50%)`
+            this.color.value = randomColor
+            return randomColor;
+        }
+    }
     _resetGrid() {
         let grids = this.parent.children
         for (let i = 0; i < grids.length; i++) {
             grids[i].style.backgroundColor = "#b7cece"
         }
+    }
+
+    _resizeGrid() {
+        this.gridSize = this.setGridSize.value
+        this._addElements()
+        this.output.textContent = this.setGridSize.value + ' x ' + this.setGridSize.value
+
     }
 }

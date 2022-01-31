b824e03b8c11e235d3d4b7a5fc400c9ae9301167
kondrei <kondrei@gmail.com>
2022-01-31 10:03:46 +0200

    adding eventListener and Reset

--- Sketch/app.js
+++ Sketch/app.js
@@ -1,25 +1,34 @@
 export class Sketck {
     constructor(gridSize) {
         this.parent = document.querySelector('.grid-container')
+        this.button = document.querySelector('#btn')
         this.gridSize = gridSize
         console.log('constructor loaded')
         this.startGame()
     }
 
     startGame() {
-        // let button = document.getElementById('btn');
-        this.addElements();
+        this._addElements();
+        this.button.addEventListener('click', () => { this._resetGrid() })
     }
 
-    addElements() {
-        for (let i = 0; i < this.gridSize; i++) {
+    _addElements() {
+        for (let i = 0; i < this.gridSize * this.gridSize; i++) {
             this.parent.style.gridTemplateColumns = `repeat(${this.gridSize}, minmax(20px,1fr))`;
             this.parent.style.gridTemplateRows = `repeat(${this.gridSize}, minmax(20px,1fr))`;
-            for (let x = 0; x < this.gridSize; x++) {
 
-                let element = document.createElement('div')
-                this.parent.appendChild(element);
-            }
+            let element = document.createElement('div')
+            element.addEventListener('mouseover', () => {
+                element.style.backgroundColor = '#6e7e85'
+            })
+            this.parent.appendChild(element);
+        }
+    }
+
+    _resetGrid() {
+        let grids = this.parent.children
+        for (let i = 0; i < grids.length; i++) {
+            grids[i].style.backgroundColor = "#b7cece"
         }
     }
 }
